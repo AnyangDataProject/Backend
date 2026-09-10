e7b771859c45638c0f5b3b6025ab2cad924c6fe9
hongdaeun11 <sally20114@gmail.com>
2026-09-10 09:06:21 +0900

    Backend 초기 설정

---
+++ anyang/src/main/java/com/dongyang/anyang/AnyangApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.anyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyangApplication.class, args);
+	}
+
+}
---
+++ anyang/src/test/java/com/dongyang/anyang/AnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyang.anyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
