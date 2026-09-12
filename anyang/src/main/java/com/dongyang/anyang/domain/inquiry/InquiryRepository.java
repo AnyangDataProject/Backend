ec9b1424127cb2098af3f0bfd78789dd630034dd
2nayeon <leenayeon0915@gmail.com>
2026-09-12 20:06:15 +0900

    feat: InquiryRepository, InquiryFileRepository 추가

--- anyang/src/main/java/com/dongyang/anyang/domain/inquiry/InquiryRepository.java
+++ anyang/src/main/java/com/dongyang/anyang/domain/inquiry/InquiryRepository.java
@@ -1,4 +1,10 @@
 package com.dongyang.anyang.domain.inquiry;
 
-public class InquiryRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.print.Pageable;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+    Page<Inquiry> findByUserId(Long userId, Pageable pageable);
 }
--- anyang/src/main/java/com/dongyang/anyang/domain/inquiry/file/InquiryRepository.java
+++ anyang/src/main/java/com/dongyang/anyang/domain/inquiry/file/InquiryRepository.java
@@ -1,4 +1,9 @@
 package com.dongyang.anyang.domain.inquiry.file;
 
-public class InquiryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InquiryRepository extends JpaRepository<InquiryFile, Long> {
+    List<InquiryFile> findByInquiryId(Long inquiryId);
 }
